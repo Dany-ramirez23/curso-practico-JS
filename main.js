5bbbb6c44c2b6eedc35a3cc79be959a71761b92c
Dany Ramírez <dany123rch@gmail.com>
2022-08-07 22:25:16 -0500

    Se agrego el carrito

--- main.js
+++ main.js
@@ -1,8 +1,26 @@
-const menuMail = document.querySelector(".navbar-email");
+const navmenu = document.querySelector(".navbar-email");
+const burguerMenu = document.querySelector(".menu");
+const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
 const desktopMenu = document.querySelector(".desktop-menu");
+const menuMobile = document.querySelector(".mobile-menu");
+const aside = document.querySelector(".product-detail");
 
-menuMail.addEventListener("click", toggleDesktopMenu);
+navmenu.addEventListener("click", toggleMenuDesktop);
+burguerMenu.addEventListener("click", toggleMenuMobile);
+menuCarritoIcon.addEventListener("click", toggleCarrito);
 
-function toggleDesktopMenu() {
+function toggleMenuDesktop() {
   desktopMenu.classList.toggle("inactive");
+  aside.classList.add("inactive");
+}
+
+function toggleMenuMobile() {
+  menuMobile.classList.toggle("inactive");
+  aside.classList.add("inactive");
+}
+
+function toggleCarrito() {
+  aside.classList.toggle("inactive");
+  menuMobile.classList.add("inactive");
+  desktopMenu.classList.add("inactive");
 }
